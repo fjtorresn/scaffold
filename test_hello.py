8379356ed43a5715096f807acbc3e04320726555
Francisco - AzureCloudShell <fco.torres12@gmail.com>
2025-12-27 20:52:10 +0000

    Add format to the files

--- test_hello.py
+++ test_hello.py
@@ -4,8 +4,10 @@ from hello import toyou, add, subtract
 def test_hello_subtract():
     assert subtract(10) == 9
 
+
 def test_hello_add():
     assert add(7) == 8
 
+
 def test_hello_toyou():
-    assert toyou('Francisco') == 'hi Francisco'
+    assert toyou("Francisco") == "hi Francisco"
